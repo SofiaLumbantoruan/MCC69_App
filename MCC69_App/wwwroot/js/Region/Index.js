b39f6a3f1fef4be8172b7766e1984e52e9199531
SofiaLumbantoruan <113152563+SofiaLumbantoruan@users.noreply.github.com>
2022-09-29 17:04:31 +0700

    Add files via upload

---
+++ MCC69_App/wwwroot/js/Region/Index.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    $("#tableRegion").dataTable({
+        "ajax": {
+            url: "/Region/GetAll",
+            type: "GET",
+            dataSrc: "",
+            dataType: "JSON"
+        },
+        "columns": [
+            {
+                "data": "",
+                "render": function (data, type, row) {
+                    console.log(row);
+                    return `${row.name}`;
+                }
+            }
+        ]
+    });
+
+});
+
+
+
+    ////$.ajax({
+    ////    type: "GET",
+    ////    url: "/Region/GetAll"
+    ////    }).done((region) => {
+    ////    console.log(region);
+    ////    let text = "";
+    ////    $.each(region, function (key, val) {
+    ////      text +=`
+    ////          <tr>
+    ////                <td>${key + 1}</td>
+    ////                <td>${val.name}</td>
+    ////                </tr>`;
+
+    ////        $("#tbodySW").html(text);
+
+    ////});    ////    }).catch((error) => {
+    ////        console.log(error)
+    ////    })
